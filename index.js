f8d21fce74179a76b9fcbd6279dcaa84a9ffbd91
Doris Morales <moralesdoris880@gmail.com>
2021-11-11 10:49:09 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+   const reversedWord = reverseString(word);
+   return word === reversedWord;
 }
-
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
 /* 
-  Add your pseudocode here
+  Add your pseudocode here: 
+  Make a function that split ups word into character
+  Then reverses the word
+  In the main function, you would call reverseWord and test it out with original word
+
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here:
+  Reverse the word and test it with the original word
 */
 
 // You can run `node index.js` to view these console logs
